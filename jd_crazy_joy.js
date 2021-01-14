f0fae2703ebb09f177cc110a0a1a5a07cbea9361
xyjsx <xyj1012@gmail.com>
2021-01-14 14:46:29 +0800

    Update jd_crazy_joy.js

--- jd_crazy_joy.js
+++ jd_crazy_joy.js
@@ -33,8 +33,9 @@ let helpSelf = false // 循环助力，默认关闭
 let applyJdBean = 0; //疯狂的JOY京豆兑换，目前最小值为2000京豆，默认为 0 不开启京豆兑换
 let cookiesArr = [], cookie = '', message = '';
 const inviteCodes = [
-  'EdLPh8A6X5G1iWXu-uPYfA==@0gUO7F7N-4HVDh9mdQC2hg==@fUJTgR9z26fXdQgTvt_bgqt9zd5YaBeE@nCQQXQHKGjPCb7jkd8q2U-aCTjZMxL3s@2boGLV7TonMex8-nrT6EGat9zd5YaBeE',
-  'EdLPh8A6X5G1iWXu-uPYfA==@0gUO7F7N-4HVDh9mdQC2hg==@fUJTgR9z26fXdQgTvt_bgqt9zd5YaBeE@nCQQXQHKGjPCb7jkd8q2U-aCTjZMxL3s@2boGLV7TonMex8-nrT6EGat9zd5YaBeE'
+  'EdLPh8A6X5G1iWXu-uPYfA==@9j6Hg1MVsaFXkIC3_NIbYg==@2UzbXnBchXYzUtN0WmyGjw==@nCQQXQHKGjPCb7jkd8q2U-aCTjZMxL3s@2boGLV7TonMex8-nrT6EGat9zd5YaBeE',
+  'EdLPh8A6X5G1iWXu-uPYfA==@sPxItqFd5LKpeO1tbG5Hzqt9zd5YaBeE@2UzbXnBchXYzUtN0WmyGjw==@nCQQXQHKGjPCb7jkd8q2U-aCTjZMxL3s@2boGLV7TonMex8-nrT6EGat9zd5YaBeE',
+  'EdLPh8A6X5G1iWXu-uPYfA==@sPxItqFd5LKpeO1tbG5Hzqt9zd5YaBeE@2UzbXnBchXYzUtN0WmyGjw==@nCQQXQHKGjPCb7jkd8q2U-aCTjZMxL3s@2boGLV7TonMex8-nrT6EGat9zd5YaBeE'
 ];
 const randomCount = $.isNode() ? 10 : 5;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
